b589356f64fa4e272cd213af17db2dfcf5a26b9a
Robu David <david@devcentrehouse.eu>
2025-10-07 08:13:51 +0300

    fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "./components/header";
 import ScrollManager from "./components/ScrollManager";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
